f6ef758929d9c084397bc17e4e7e0f000e7c4668
lohoa <lohoarpan@gmail.com>
2021-05-25 15:35:01 +0530

    Creating feature modules with routing

---
+++ src/app/modules/feature-module-a/feature-module-a.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FeatureModuleARoutingModule } from './feature-module-a-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FeatureModuleARoutingModule
+  ]
+})
+export class FeatureModuleAModule { }
---
+++ src/app/modules/feature-module-b/feature-module-b.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FeatureModuleBRoutingModule } from './feature-module-b-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FeatureModuleBRoutingModule
+  ]
+})
+export class FeatureModuleBModule { }
