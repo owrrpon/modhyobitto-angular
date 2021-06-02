e0c7df4e37be797571dda94c75f2590b977ae25e
lohoa <lohoarpan@gmail.com>
2021-06-02 20:55:49 +0530

    sticky header

--- src/app/global-components/header/header.component.ts
+++ src/app/global-components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  is_page_scrolled: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('window:scroll', ['$event.currentTarget.pageYOffset'])
+  handleStickyNavigation(sroll_distance: Number){
+    this.is_page_scrolled = sroll_distance >= 150;
   }
 
 }
