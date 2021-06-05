c939de2091b47a522f996da9765c9045ea899261
lohoa <lohoarpan@gmail.com>
2021-06-05 22:56:00 +0530

    added the loader

--- src/app/app-material-importer.module.ts
+++ src/app/app-material-importer.module.ts
@@ -3,12 +3,14 @@ import {MatTabsModule} from '@angular/material/tabs';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import { MatRippleModule } from '@angular/material/core';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 const MATERIAL_COMPONENTS = [
   MatTabsModule,
   MatInputModule,
   MatSelectModule,
-  MatRippleModule
+  MatRippleModule,
+  MatProgressSpinnerModule
 ];
 
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { AppUtilityService } from './app-utility.service';
 
 @Component({
@@ -8,14 +9,43 @@ import { AppUtilityService } from './app-utility.service';
 })
 export class AppComponent implements OnInit, AfterViewInit, OnDestroy{
 
+  is_loader_showing: boolean = false;
+  loading_animation_control_sub!: Subscription;
+
   constructor(
     public global_utilities: AppUtilityService
   ){}
 
   ngOnInit(){}
 
-  ngAfterViewInit(){}
+  ngAfterViewInit(){
+    let loader_control = this.global_utilities.getGlobalData('loading_animation_control');
+    this.loading_animation_control_sub = loader_control.subscribe(
+      (to_show: any) => {
+        // Show if the loader is not being shown already
+        if(to_show && !this.is_loader_showing){
+          this.is_loader_showing = true;
+        }
+        // Hide if the loader is being shown and there is no ongoing service call in next few seconds
+        if(!to_show && this.is_loader_showing){
+          let ongoing_call_check_interval = setInterval(
+            ()=>{
+              let ongoing_call_count = this.global_utilities.getGlobalData('ongoing_request_count');
+              if(ongoing_call_count == 0){
+                this.is_loader_showing = false;
+                clearInterval(ongoing_call_check_interval);
+              }
+            }, 1000
+          );
+        }
+      }
+    );
+  }
 
-  ngOnDestroy(){}
+  ngOnDestroy(){
+    this.global_utilities.unsubscribeAll([
+      this.loading_animation_control_sub
+    ]);
+  }
 
 }
